48f1020aa3580f56c5746f2baf256a0712235a98
Tianyi Wang <wang103uiuc@gmail.com>
2012-10-29 21:29:17 -0500

    Added PHP for blog

--- blog.php
+++ blog.php
@@ -23,15 +23,20 @@ Tianyi Wang's Homepage
 
 <br>
 
-<p>Email: wang103 <i>AT</i> illinois <i>DOT</i> edu</p>
+<div id="blog_left">
+<?php include 'php/load_blog.php'; ?>
+</div>
 
-<p>QQ: 1728420693</p>
-
-<p><a href="https://www.facebook.com/tianyi.wang1" target="_blank">Facebook</a></p>
-
-<p><a href="http://www.linkedin.com/pub/tianyi-wang/38/991/75" target="_blank">LinkedIn</a></p>
+<div id="blog_right">
+<form name="login_form" action="php/actions.php" method="get">
+<p id="login_p"><label id="login_label">User: </label> <input id="login_input" type="text" name="user" required></p>
+<p id="login_p"><label id="login_label">Password:</label> <input id="login_input" type="password" name="pwd" required></p>
+<p id="login_p" align="center"><input type="submit" value="Submit"></p>
+</form>
+</div>
 
 <!--Footer-->
+<br>
 <br>
 <div id="footer">
 &copy; 2012 Tianyi Wang
---
+++ php/actions.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+<?php
+echo "Coming soon";
+?>
+
+</body>
+</html>
