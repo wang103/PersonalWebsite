dfa96d802781fd51f9d065cd55c7e63a84fba65a
Tianyi Wang <wang103uiuc@gmail.com>
2012-11-02 03:52:48 -0500

    Fixed a bug

--- php/load_blog.php
+++ php/load_blog.php
@@ -1,3 +1,16 @@
 <?php
 echo "<p>Blog is coming soon</p>";
+
+# Get the current page number.
+if (!isset($_GET["pn"]) || empty($_GET["pn"])) {
+	$page_number = 1;
+} else {
+	$page_number = $_GET["pn"];
+}
+
+# Display the blogs that should appear on the current page.
+
+
+# Display at most 7 pages centering the current page.
+
 ?>
